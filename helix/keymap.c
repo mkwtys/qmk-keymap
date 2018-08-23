9d4ecc86769e0dd28de28326da31dae25ff895ac
mkwtys <mkwtys@gmail.com>
2018-08-23 13:50:40 +0900

    remove unnecessary fillers

--- helix/keymap.c
+++ helix/keymap.c
@@ -39,9 +39,6 @@ enum custom_keycodes {
   RGBRST
 };
 
-// Fillers to make layering more clear
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Qwerty
    * ,-----------------------------------------.             ,-----------------------------------------.
