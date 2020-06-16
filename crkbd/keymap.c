26891c1ee479c26ca2b76cf5e4719df2f32e913c
mkwtys <mkwtys@gmail.com>
2020-06-16 23:29:41 +0900

    crkbd: diabled rgb

--- crkbd/keymap.c
+++ crkbd/keymap.c
@@ -1,10 +1,5 @@
 #include QMK_KEYBOARD_H
 
-#ifdef RGBLIGHT_ENABLE
-// Following line allows macro to read current RGB settings
-extern rgblight_config_t rgblight_config;
-#endif
-
 extern uint8_t is_master;
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
@@ -20,9 +15,7 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST,
-  BACKLIT,
-  RGBRST
+  ADJUST
 };
 
 enum macro_keycodes {
@@ -79,42 +72,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                         KC_GUIEI,LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_ALTKN \
   ),
   // ,-----------------------------------------.                ,-----------------------------------------.
-  // | reset|rgbrst|      |      |      |      |                |      |      |      |      |      |      |
+  // | reset|      |      |      |      |      |                |      |      |      |      |      |      |
   // |------+------+------+------+------+------|                |------+------+------+------+------+------|
-  // |rbg on| hue+ | sai+ | val+ |      |      |                |      |      |      |      |      |      |
+  // |      |      |      |      |      |      |                | left | down |   up | right|      |      |
   // |------+------+------+------+------+------+                +------+------+------+------+------+------|
-  // | mode | hue- | sai- | val- |      |      |                |      |      |      |      |      |      |
+  // |      |      |      |      |      |      |                |      |      |      |      |      |      |
   // `---------------------------+------+------+------.  ,------+-----------------------------------------'
   //                             |gui/ei| lower| space|  | enter| raise|alt/kn|
   //                             `--------------------'  '--------------------'
   [_ADJUST] = LAYOUT( \
-    RESET,   RGBRST,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-    RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
-    RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+    RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
                                         KC_GUIEI,LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_ALTKN \
   )
 };
 
-int RGB_current_mode;
-
-void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
-}
-
-// Setting ADJUST layer RGB back to default
-void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-  if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-    layer_on(layer3);
-  } else {
-    layer_off(layer3);
-  }
-}
-
 void matrix_init_user(void) {
-#ifdef RGBLIGHT_ENABLE
-  RGB_current_mode = rgblight_config.mode;
-#endif
 // SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
 #ifdef SSD1306OLED
   iota_gfx_init(!has_usb());  // turns on the display
@@ -178,25 +152,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-        persistent_default_layer_set(1UL << _QWERTY);
+        eeconfig_update_default_layer(1UL << _QWERTY);
+        default_layer_set(1UL << _QWERTY);
       }
       return false;
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       }
       return false;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
-        update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
       }
       return false;
     case ADJUST:
@@ -206,24 +177,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_ADJUST);
       }
       return false;
-    case RGB_MOD:
-#ifdef RGBLIGHT_ENABLE
-      if (record->event.pressed) {
-        rgblight_mode(RGB_current_mode);
-        rgblight_step();
-        RGB_current_mode = rgblight_config.mode;
-      }
-#endif
-      return false;
-    case RGBRST:
-#ifdef RGBLIGHT_ENABLE
-      if (record->event.pressed) {
-        eeconfig_update_rgblight_default();
-        rgblight_enable();
-        RGB_current_mode = rgblight_config.mode;
-      }
-#endif
-      break;
   }
   return true;
 }
